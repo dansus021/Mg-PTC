d72ed85eead4d884f443a9a9ba4fd9fb548910b8
M. Sean Adam Z <dansus021@users.noreply.github.com>
2016-10-16 20:07:29 +0700

    Add files via upload

---
+++ asset/footer.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="includes/js/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="includes/js/bootstrap.min.js"></script>
+<!-- SweetAlert -->
+<script src="includes/sweetalert-master/dist/sweetalert-dev.js"></script>
---
+++ asset/headuser.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="PTC Monitor - PTC Review - PTC Payment Proof - New PTC">
+    <meta name="author" content="Mg-Project Indonesia">
+
+    <title>Mg-PTC - Earning from PTC Safely</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="includes/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="includes/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link href="includes/css/shop-item.css" rel="stylesheet">
+
+    <!-- Sweetalert -->
+    <link rel="stylesheet" href="includes/sweetalert-master/dist/sweetalert.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Mg-PTC</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#" >Home Monitor &amp; Review</a>
+                    </li>
+                    <li>
+                        <a href="#">Scam Sites</a>
+                    </li>
+                    <li>
+                        <a href="#">Add New PTC Site</a>
+                    </li>
+                    <li>
+                        <a href="#">F.A.Q</a>
+                    </li>
+                    <li>
+                        <a href="#">TOS</a>
+                    </li>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="#">Cookie Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Our Other Site</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
