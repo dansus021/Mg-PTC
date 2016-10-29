f470be7e23453a273b84a594c00ccfea2be1990f
M. Sean Adam Z <dansus021@users.noreply.github.com>
2016-10-29 07:07:06 +0700

    Add files via upload

---
+++ asset/footeruser.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="includes/js/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="includes/js/bootstrap.min.js"></script>
+<!-- Disqus -->
+<script id="dsq-count-scr" src="//mgptc.disqus.com/count.js" async></script>
+<!-- User JS -->
+<script type="text/javascript" src="includes/user.js"></script>
+<!-- Recaptcha -->
+<script src='https://www.google.com/recaptcha/api.js'></script>
--- asset/headuser.php
+++ asset/headuser.php
@@ -20,8 +20,15 @@
     <!-- Custom CSS -->
     <link href="includes/css/shop-item.css" rel="stylesheet">
 
-    <!-- Sweetalert -->
-    <link rel="stylesheet" href="includes/sweetalert-master/dist/sweetalert.css">
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <script type="text/javascript">
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://mg-ptc.net/cookie-policy","theme":"dark-floating"};
+    </script>
+
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
+
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -45,41 +52,172 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Mg-PTC</a>
+                <a class="navbar-brand" href="index">Mg-PTC</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="#" >Home Monitor &amp; Review</a>
+                        <a href="index" >Home Monitor &amp; Review</a>
                     </li>
                     <li>
-                        <a href="#">Scam Sites</a>
+                        <a href="scam-sites">Scam Sites</a>
                     </li>
                     <li>
-                        <a href="#">Add New PTC Site</a>
+                        <a href="#" data-toggle="modal" data-target="#addptc">Add New PTC Site</a>
                     </li>
                     <li>
-                        <a href="#">F.A.Q</a>
+                        <a href="faq">F.A.Q</a>
                     </li>
                     <li>
-                        <a href="#">TOS</a>
+                        <a href="tos">TOS</a>
                     </li>
                     <li>
-                        <a href="#">About Us</a>
+                        <a href="cookie-policy">Cookie Policy</a>
                     </li>
                     <li>
-                        <a href="#">Contact Us</a>
+                        <a href="#" data-toggle="modal" data-target="#contacus">Contact Us</a>
                     </li>
                     <li>
-                        <a href="#">Cookie Policy</a>
+                        <a href="#" data-toggle="modal" data-target="#aboutus">About Us</a>
                     </li>
-                    <li>
-                        <a href="#">Our Other Site</a>
-                    </li>
+                    
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
         </div>
         <!-- /.container -->
     </nav>
+
+<!-- Add PTC Modal -->
+<form action="proses?action=adduser" method="POST" name="addptc" class="form-horizontal" role="form">
+<div class="modal fade" id="addptc" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-plus"></i> Add New PTC Site!...</h4>
+      </div>
+      <div class="modal-body">
+      <?php //echo $error; ?>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Site Name:</strong>
+            <input type="text" class="form-control"  name="user" maxlength="20" placeholder="Username Anda..." required>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Site URL:</strong>
+            <input type="text" class="form-control"  name="user" maxlength="20" placeholder="Username Anda..." required>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Other Info (Optional):</strong>
+            <p><font size="1%">(Max Character: 200)</font></p>
+            <textarea name="otherinfo" id="alamatcuy" class="form-control" rows="5" placeholder="Other Info. Anything info about this PTC. "></textarea>
+            <font size="1%"><span id="hitung2" class="text-right">200</span>&nbsp;Character left</font>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Prove you're human <i class="fa fa-smile-o"></i> :</strong>
+            <div class="g-recaptcha" data-sitekey="6LdvlQkUAAAAABvuN_j22KnaQUPtt0uZBXVmZCn2"></div>
+          </div>
+        </div><p></p>
+        <div class="row text-right" style="margin-top:1%;">
+          <div class="col-sm-12 controls">
+            <button type="submit" name="Submit" class="btn btn-primary"><i class="fa fa-send"></i> | Send</button>
+            <button type="reset" name="Clear" class="btn btn-warning"><i class="fa fa-trash-o"></i> | Clear</button>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        Copyright &copy; Mg-PTC
+        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<!-- End Of Add PTC Modal -->
+
+<!-- Contac Us Modal -->
+<form action="proses?action=adduser" method="POST" name="contacus" class="form-horizontal" role="form">
+<div class="modal fade" id="contacus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-phone"></i> Contact Us!...</h4>
+      </div>
+      <div class="modal-body">
+      <?php //echo $error; ?>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Name:</strong>
+            <input type="text" class="form-control"  name="user" maxlength="20" placeholder="Your Name..." required>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Email:</strong>
+            <input type="text" class="form-control"  name="user" maxlength="20" placeholder="Your Email..." required>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Information:</strong>
+            <p><font size="1%">(Max Character: 200)</font></p>
+            <textarea name="otherinfo" id="alamatcuy" class="form-control" rows="5" placeholder="Critics, Advice or Other Information. Please feel free to contact us."></textarea>
+            <font size="1%"><span id="hitung2" class="text-right">200</span>&nbsp;Character left</font>
+          </div>
+        </div><p></p>
+        <div class="row">
+          <div class="col-md-12">
+            <strong>Prove you're human <i class="fa fa-smile-o"></i> :</strong>
+            <div class="g-recaptcha" data-sitekey="6LdvlQkUAAAAABvuN_j22KnaQUPtt0uZBXVmZCn2"></div>
+          </div>
+        </div><p></p>
+        <div class="row text-right" style="margin-top:1%;">
+          <div class="col-sm-12 controls">
+            <button type="submit" name="Submit" class="btn btn-primary"><i class="fa fa-send"></i> | Send</button>
+            <button type="reset" name="Clear" class="btn btn-warning"><i class="fa fa-trash-o"></i> | Clear</button>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        Copyright &copy; Mg-PTC
+        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<!-- End Of Contac Us Modal -->
+
+<!-- About Us Modal -->
+<div class="modal fade" id="aboutus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-bell-o"></i> Please visit our other site :)</h4>
+      </div>
+      <div class="modal-body">
+      <?php //echo $error; ?>
+        <div class="panel panel-info">
+            <div class="panel-heading"><i class="fa fa-info"></i> About Us</div>
+            <div class="panel-body">
+                <p> Mg-PTC is a site that is useful to you all who want to plunge into the world of PTC. Starting from the many PTC sites that eventually became a scam for many reasons. We will always provide information about existing PTC. Support Us. </p>
+                <p>Don't forget to Visit other Mg-Project program</p>
+                <p><i class="fa fa-check"></i>Merbot.io</p>
+            </div>
+            <div class="panel-footer">Copyright &copy; 2016 <a href="">Mg-PTC</a>. Powered By <a href="">Mg-Project Indonesia</a> &amp; <a href="https://merbot.io">Merbot.io</a>. All Right Reserved.</div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Of About Us Modal -->
